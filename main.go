a4ba50003c49b31771b8003b49024dbca14b6367
Philipp Antar <philipp.antar@deliveryhero.com>
2020-11-17 00:06:27 +0100

    update

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		w.Write([]byte("Hello World, have a great day!"))
 	})
 
 	port := os.Getenv("PORT")
